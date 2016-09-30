b6f4db8e06ebb21cb03b19b955f620c59e16051b
arith-Harrell <m.harrell@arith-metic.jp>
2016-09-30 12:37:53 +0900

    #6 created game root folder

---
+++ sample/QuizGame/data/question.js
@@ -0,0 +1,392 @@
+var quizQuestions = [{
+	"q1" : [{
+		"text" : {
+			"en" : "What do you like the most about me?",
+			"jp" : "私のどこが一番いいですか？"
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "Legs",
+					"jp" : "足"
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "Dancing",
+					"jp" : "ダンシング！"
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "Voice",
+					"jp" : "声"
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "Hair",
+					"jp" : "髪"
+				},
+				"correct" : false 	
+			}]									
+	}],
+	"q2" : [{
+		"text" : {
+			"en" : "OH YES! MTT-BRAND OVENS! ROASTING! TOASTING! BURNING! CHARRING! HOW HOT CAN THEY GET, DARLING?",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "UP TO NINE THOUSAND DEGREES!",
+					"jp" : "9000°"
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "OVER NINE THOUSAND!",
+					"jp" : "八千以上だ!"
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "1.41679 x 10^32 Kelvin",
+					"jp" : "1.41679 x 10^32 ケルビン"
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "451° f",
+					"jp" : "451° 華氏"
+				},
+				"correct" : false 	
+			}]
+	}],
+	"q3" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]
+	}],
+	"q4" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q5" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q6" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q7" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q8" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q9" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}],
+	"q10" : [{
+		"text" : {
+			"en" : "",
+			"jp" : ""
+		},
+		"answers" : [
+			{
+				"aID" : 0,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : true
+			},
+			{
+				"aID" : 1,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false	
+			},
+			{
+				"aID" : 2,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			},
+			{
+				"aID" : 3,
+				"text" : {
+					"en" : "",
+					"jp" : ""
+				},
+				"correct" : false 	
+			}]	
+	}]									
+}]
