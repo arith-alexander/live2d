1fee0ef9114dc7f9db28d09069d34b69a1d2cb0b
arith-Harrell <m.harrell@arith-metic.jp>
2016-10-07 18:51:29 +0900

    #25 point get

--- sample/QuizGame/data/question.js
+++ sample/QuizGame/data/question.js
@@ -12,7 +12,7 @@ var quizQuestions = [{
 					"jp" : "足"
 				},
 				"correct" : true,
-				"points" : 500
+				"points" : 1000
 			},
 			{
 				"aID" : 1,
@@ -286,7 +286,6 @@ function Quiz() {
 function populateQuestions(dataObject){
 	var questionArray = [];
 	for(var key in dataObject){
-		console.log("??");
 		var question = {};
 		question.text = dataObject[key][0]["text"];
 		question.answers = dataObject[key][0]["answers"];
@@ -294,4 +293,36 @@ function populateQuestions(dataObject){
 	}
 	return questionArray;
 }
+
+function displayScore(score){
+	$("#score").html(score);
+}
+
+function updateScore(points, quiz){
+	currentScore = quiz.score;
+	newScore= points + currentScore;
+	quiz.score = newScore;
+	return newScore;
+}
+
+function displayQuestion(question, q= quizObj){
+	console.log(q);
+	var answers = question["answers"];
+	$("#question .shade p").html(question.text.jp);
+	answerboxes = $("#answerbox ul li");
+	$.each(answerboxes, function(){
+		console.log(q);
+		thisanswer = answers.pop();
+		$(this).html(thisanswer.text.jp);
+		$(this).attr("data-score", thisanswer.points);
+		$(this).attr("data-aID", 0);
+		$(this).click(function(){
+			var points = parseInt($(this).attr("data-score"));
+			displayScore(updateScore(points, q));
+		});
+
+
+	})
+}
+
 
